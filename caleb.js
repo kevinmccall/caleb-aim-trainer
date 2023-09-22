2cb84a142f82833cdf11278e8bb9da5ac6a6eea8
ErakiJeff <erakiproj99@gmail.com>
2023-09-21 23:00:38 -0400

    going to make clean up all this clutter

--- caleb.js
+++ caleb.js
@@ -1,27 +1,16 @@
 import { isPointInRect } from "./utils.js";
 
-
-
-
-export function CalebObj(engine) {
+export function CalebObj(img, width, height) {
   this.x = 0;
   this.y = 0;
-  this.dx = 0;
-  this.dy = 0;
   this.scaleX = 1;
   this.scaleY = 1;
   this.growing = true;
-  this.image = new Image(CALEB_WIDTH, CALEB_HEIGHT);
-  this.image.src = CALEB_IMG_PATH;
-  this.totalWidth = CALEB_WIDTH;
-  this.totalHeight = CALEB_HEIGHT;
+  this.image = img;
+  this.totalWidth = width;
+  this.totalHeight = height;
   this.calebID = null;
-  this.engine = engine;
-  this.onclick = null;
-  this.ondeath = null;
 }
-
-
 
 CalebObj.prototype.update = function(delta) {
   if (isPointInRect(this.engine.mouseX, this.engine.mouseY,
@@ -38,3 +27,24 @@ CalebObj.prototype.update = function(delta) {
   }
 };
 
+CalebObj.prototype.onClick = function(delta) {
+  if (isPointInRect(this.engine.mouseX, this.engine.mouseY,
+    this.getVisualX(),
+    this.getVisualY(),
+    this.totalWidth * this.scaleX,
+    this.totalHeight * this.scaleY
+  ) && this.engine.isMouseClicked) {
+    this.engine.isMouseClicked = false;
+    this.engine.queueUnregisterEntity(this.calebID);
+    if (this.onclick != null && typeof this.onclick === 'function') {
+      this.onclick();
+    }
+  }
+};
+
+CalebObj.prototype.onDisappear = function(delta) {
+  this.engine.queueUnregisterEntity(this.calebID);
+  if (this.onclick != null && typeof this.onclick === 'function') {
+    this.onclick();
+  }
+}
--- engine.js
+++ engine.js
@@ -1,51 +1,51 @@
 import { getLivesLeft } from "./scorecounter.js";
 
 export class Engine {
-  constructor() {
-    this.canvas = document.getElementById("screen");
-    this.ctx = this.canvas.getContext("2d");
-    this.updatedObjects = new Map();
-    this.old = new Date().getTime();
-    this.canvas.width = window.innerWidth - 10;
-    this.canvas.height = window.innerHeight - 10;
-    this.toDelete = [];
-    this.currentElementID = 0;
-    this.playing = true;
-  }
-  registerEntity(entity) {
-    this.updatedObjects.set(this.currentElementID, entity);
-    this.currentElementID += 1;
-    return this.currentElementID - 1;
-  }
-  queueUnregisterEntity(id) {
-    this.toDelete.push(id);
-  }
-  update() {
-    let current = new Date().getTime();
-    let delta = (current - this.old) / 1000;
-    this.old = current;
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.updatedObjects.forEach((obj) => {
-      if (typeof (obj.update) === 'function') {
-        obj.update(delta);
-      }
-      if (typeof (obj.draw) === 'function') {
-        obj.draw(this.ctx);
-      }
-    });
-    if (this.toDelete.length > 0) {
-      this.toDelete.forEach((id) => {
-        delete this.updatedObjects.get(id);
-        this.updatedObjects.delete(id);
-      });
-      this.toDelete = [];
+    constructor() {
+        this.canvas = document.getElementById("screen");
+        this.ctx = this.canvas.getContext("2d");
+        this.updatedObjects = new Map();
+        this.old = new Date().getTime();
+        this.canvas.width = window.innerWidth - 10;
+        this.canvas.height = window.innerHeight - 10;
+        this.toDelete = [];
+        this.currentElementID = 0;
+        this.playing = true;
     }
-    if (getLivesLeft() <= 0) {
-      this.playing = false;
+    registerEntity(entity) {
+        this.updatedObjects.set(this.currentElementID, entity);
+        this.currentElementID += 1;
+        return this.currentElementID - 1;
     }
-    if (this.playing) {
-      requestAnimationFrame(this.update.bind(this));
+    queueUnregisterEntity(id) {
+        this.toDelete.push(id);
     }
-  }
+    update() {
+        let current = new Date().getTime();
+        let delta = (current - this.old) / 1000;
+        this.old = current;
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.updatedObjects.forEach((obj) => {
+            if (typeof (obj.update) === 'function') {
+                obj.update(delta);
+            }
+            if (typeof (obj.draw) === 'function') {
+                obj.draw(this.ctx);
+            }
+        });
+        if (this.toDelete.length > 0) {
+            this.toDelete.forEach((id) => {
+                delete this.updatedObjects.get(id);
+                this.updatedObjects.delete(id);
+            });
+            this.toDelete = [];
+        }
+        if (getLivesLeft() <= 0) {
+            this.playing = false;
+        }
+        if (this.playing) {
+            requestAnimationFrame(this.update.bind(this));
+        }
+    }
 }
 
